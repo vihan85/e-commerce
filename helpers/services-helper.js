a29f349473ac1a52a7b53bcd689c129082534608
vihan85 <hanthuyvi0805@gmail.com>
2022-12-31 13:02:34 +0700

    comment useDebounce

--- helpers/services-helper.js
+++ helpers/services-helper.js
@@ -2,7 +2,7 @@ import { httpRequest } from '../until/http-request';
 
 /**
  *
- * @param {function} serviceProductList => loop services call API ưith another path
+ * @param {function} serviceProductList => loop services call API with another path
  * @param {Object} params => all parameters
  * @returns {Promise} =>arrayPromise (path:product, path:price, path:image)
  *
