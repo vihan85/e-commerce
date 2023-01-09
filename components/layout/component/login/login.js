09345954c4eb19bfebd1d0ef70db773a8dd93d07
vihan85 <hanthuyvi0805@gmail.com>
2023-01-10 01:39:19 +0700

    validate-when-onBlure

--- components/layout/component/login/login.js
+++ components/layout/component/login/login.js
@@ -6,20 +6,14 @@ const cx = classNames.bind(styles);
 function Login() {
     const [inputEmailValue, setInputEmailValue] = useState('');
     const [inputPassValue, setInputPassValue] = useState('');
-    const [inputconfirmPassValue,setInputconfirmPassValue] = useState('')
+    const [inputconfirmPassValue, setInputconfirmPassValue] = useState('');
     const emailValueRef = useRef();
     const passWordlValueRef = useRef();
-    const confirmPassWordlValueRef = useRef()
+    const confirmPassWordlValueRef = useRef();
     //validate
     const [auth, setAuth] = useState();
-    const handleOnSubmit = (e) => {
-        e.preventDefault();
-        const validateValues = {
-            email: emailValueRef.current.value,
-            pass: passWordlValueRef.current.value,
-            confirmPassWord: confirmPassWordlValueRef.current.value
-        };
-        fetch('api/validate', {
+    const api = (validateValues) => {
+        fetch('api/auth', {
             method: 'POST',
             body: JSON.stringify(validateValues),
             headers: {
@@ -31,6 +25,33 @@ function Login() {
                 setAuth(dataerror);
             });
     };
+    const handleOnSubmit = (e) => {
+        e.preventDefault();
+        const validateValues = {
+            email: emailValueRef.current.value,
+            pass: passWordlValueRef.current.value,
+            confirmPassWord: confirmPassWordlValueRef.current.value,
+        };
+        api(validateValues);
+    };
+    const handleBlur = (id, ref) => {
+        const keyId = id.split('-')[id.split('-').length - 1];
+        if (id.includes(keyId) && keyId !== 'confirmPassWord') {
+            const validateValues = {
+                [keyId]: ref.current.value,
+            };
+
+            api(validateValues);
+        }
+        if (id.includes(keyId) && keyId == 'confirmPassWord') {
+            const validateValues = {
+                [keyId]: ref.current.value,
+                pass: passWordlValueRef.current.value,
+            };
+
+            api(validateValues);
+        }
+    };
 
     return (
         <div className={cx('login')}>
@@ -38,9 +59,10 @@ function Login() {
                 <form
                     onSubmit={handleOnSubmit}
                     className={cx('form-1')}>
-                    <div className={cx('form-group')}>
-                        <label>Email</label>
+                    <div className={cx('form-group', 'required')}>
+                        <label htmlFor='login-form-email'>Email</label>
                         <input
+                            id='login-form-email'
                             className={cx({ invalid: auth && auth.status === 'error' && auth.error.email && true })}
                             ref={emailValueRef}
                             placeholder="Your's Email"
@@ -48,12 +70,19 @@ function Login() {
                             onChange={(e) => {
                                 setInputEmailValue(e.target.value);
                             }}
+                            onBlur={(e) => {
+                                handleBlur(e.target.getAttribute('id'), emailValueRef);
+                            }}
                         />
                         <span>{auth && auth.status === 'error' && auth.error.email}</span>
                     </div>
-                    <div className={cx('form-group')}>
-                        <label>Password</label>
+                    <div className={cx('form-group', 'required')}>
+                        <label htmlFor='login-form-pass'>Password</label>
                         <input
+                            onBlur={(e) => {
+                                handleBlur(e.target.getAttribute('id'), passWordlValueRef);
+                            }}
+                            id='login-form-pass'
                             className={cx({ invalid: auth && auth.status === 'error' && auth.error.pass && true })}
                             ref={passWordlValueRef}
                             value={inputPassValue}
@@ -64,16 +93,20 @@ function Login() {
                         />
                         <span>{auth && auth.status === 'error' && auth.error.pass}</span>
                     </div>
-                    <div className={cx('form-group')}>
-                        <label>Confirm password</label>
+                    <div className={cx('form-group', 'required')}>
+                        <label htmlFor='login-form-confirmPassWord'>Confirm password</label>
                         <input
-                            className={cx({ invalid: auth && auth.status === 'error' && auth.error.confirmPass &&  true })}
+                            onBlur={(e) => {
+                                handleBlur(e.target.getAttribute('id'), confirmPassWordlValueRef);
+                            }}
+                            id='login-form-confirmPassWord'
+                            className={cx({ invalid: auth && auth.status === 'error' && auth.error.confirmPass && true })}
                             ref={confirmPassWordlValueRef}
                             value={inputconfirmPassValue}
                             onChange={(e) => {
                                 setInputconfirmPassValue(e.target.value);
                             }}
-                            placeholder="Confirm your password"
+                            placeholder='Confirm your password'
                         />
                         <span>{auth && auth.status === 'error' && auth.error.confirmPass}</span>
                     </div>
