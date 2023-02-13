021766601c6662b84577343ded9fe1f562de280f
vihan85 <hanthuyvi0805@gmail.com>
2023-02-13 11:31:55 +0700

    fix-render-catelogyPage

--- pages/[catelory].js
+++ pages/[catelory].js
@@ -1,8 +1,20 @@
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 import CateloryPage from '~/components/pages/catelories-page/catelory-page';
-import LoadingSpinner from '~/components/ui/loading-spinner';
 import serviceCatelory from '~/services/service-catelory';
 
-function ProductPage({ dataProducts, cateloryId }) {
+function ProductPage() {
+    // { dataProducts, cateloryId }
+    const [dataProducts, setdataProducts] = useState();
+    const router = useRouter();
+    const cateloryId = router.query.catelory;
+    useEffect(() => {
+        serviceCatelory().then((res) => {
+            const data = res.find((item) => item.c_id === cateloryId);
+            setdataProducts(data);
+        });
+    }, [cateloryId]);
+
     return (
         <div className='container'>
             <CateloryPage
@@ -13,28 +25,29 @@ function ProductPage({ dataProducts, cateloryId }) {
     );
 }
 export default ProductPage;
-export async function getStaticPaths() {
-    const data = await serviceCatelory();
-    const cateloriesId = data.map((cateloryId) => ({
-        params: {
-            catelory: cateloryId.c_id,
-        },
-    }));
-    return {
-        paths: cateloriesId,
-        fallback: true, // can also be true or 'blocking'
-    };
-}
+// export async function getStaticPaths() {
+//     const data = await serviceCatelory();
+//     const cateloriesId = data.map((cateloryId) => ({
+//         params: {
+//             catelory: cateloryId.c_id,
+//         },
+//     }));
+//     return {
+//         paths: cateloriesId,
+//         fallback: true, // can also be true or 'blocking'
+//     };
+// }
 
 // `getStaticPaths` requires using `getStaticProps`
-export async function getStaticProps(context) {
-    const data = await serviceCatelory();
-    const cateloryId = context.params.catelory;
-    if (data !== undefined) {
-        const dataProductCatelory = data.find((item) => item.c_id === cateloryId);
-        return {
-            // Passed to the page component as props
-            props: { dataProducts: dataProductCatelory, cateloryId },
-        };
-    }
-}
+
+// export async function getStaticProps(context) {
+//     const data = await serviceCatelory();
+//     const cateloryId = context.params.catelory;
+//     if (data !== undefined) {
+//         const dataProductCatelory = data.find((item) => item.c_id === cateloryId);
+//         return {
+//             // Passed to the page component as props
+//             props: { dataProducts: dataProductCatelory, cateloryId },
+//         };
+//     }
+// }
